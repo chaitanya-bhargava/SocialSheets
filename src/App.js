ace35508f627744eacb56d131e15f5b80580a1f9
chaitanya-bhargava <chaitanyabhargava5@gmail.com>
2024-08-30 00:33:16 +0530

    stuff

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import "@silevis/reactgrid/styles.css";
+import Spreadsheet from './components/CustomSpreadsheet/CustomSpreadsheet';
+
+
+const App = () => {
+  return <Spreadsheet/>
+}
+
+export default App;
