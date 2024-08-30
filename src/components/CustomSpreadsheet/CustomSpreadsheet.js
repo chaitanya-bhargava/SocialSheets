e4e97ec3b46c4f8a3726767be0d756b9ce2c23d1
chaitanya-bhargava <chaitanyabhargava5@gmail.com>
2024-08-30 20:25:14 +0530

    user collab support added

--- src/components/CustomSpreadsheet/CustomSpreadsheet.js
+++ src/components/CustomSpreadsheet/CustomSpreadsheet.js
@@ -1,9 +1,14 @@
 import Spreadsheet from 'react-spreadsheet';
-import { useState, useEffect } from 'react';
+import { useState, useEffect} from 'react';
 import supabase from '../../supabase';
 
 const CustomSpreadsheet = () => {
   const [cells, setCells] = useState([]);
+  const [activeCell,setActiveCell] = useState({value:""});
+  const [activeCellCoords,setActiveCellCoords] = useState({
+    row:0,
+    column:0
+  })
 
   const handleInsert = (payload) => {
     console.log('Insert received!', payload);
@@ -24,7 +29,7 @@ const CustomSpreadsheet = () => {
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await supabase.from('spreadsheets').select('cells').eq('id',1);
-
+      setActiveCell(data[0].cells.cells[0][0]);
       setCells(data[0].cells.cells);
     };
 
@@ -65,14 +70,44 @@ const CustomSpreadsheet = () => {
     subscribeToCellChanges();
   }, []);  
   
-  const handleChanges = async (newCells) => {
-    await supabase.from('spreadsheets').update({cells:{
-      cells:newCells
-    }}).eq('id',1).select();
-    debugger
+  
+
+  const handleChanges = async ({row,column}) => {
+    if(activeCell.value!==""){
+      setCells(prevCells => {
+        const updatedCells = [...prevCells];
+        updatedCells[activeCellCoords.row][activeCellCoords.column] = activeCell;
+        return updatedCells;
+      });
+    }
+    setActiveCell({value:""});
+    setActiveCellCoords({row,column});
   }
 
-  return <Spreadsheet data={cells} onChange={handleChanges}/>;
+  useEffect(()=>{
+    const updateSupaBase = async () => {
+      if(cells.length>0){
+        await supabase.from('spreadsheets').update({cells:{
+          cells:cells
+        }}).eq('id',1).select();
+      }
+    }
+    updateSupaBase();
+  },[activeCellCoords])
+
+  const handleKeyChanges = (event) =>{
+    if (event.keyCode >= 48 && event.keyCode <= 57) {
+        setActiveCell({value:event.target.value})
+    } else if (event.keyCode >= 65 && event.keyCode <= 90) {
+        setActiveCell({value:event.target.value})
+    } else if (event.keyCode >= 97 && event.keyCode <= 122) {
+        setActiveCell({value:event.target.value})
+    } else if (event.key==="Enter"){
+        setActiveCell({value:event.target.value})
+    }
+  }
+
+  return <Spreadsheet data={cells} onActivate={handleChanges} onKeyDown={handleKeyChanges}/>;
 }
 
 export default CustomSpreadsheet;
