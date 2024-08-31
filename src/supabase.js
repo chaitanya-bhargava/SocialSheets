28076aad44fbe8e22fe5a994d97f61e997b76691
chaitanya-bhargava <chaitanyabhargava5@gmail.com>
2024-09-01 00:51:47 +0530

    environment variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zwbriswstaddmusjunqb.supabase.co/';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3YnJpc3dzdGFkZG11c2p1bnFiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNDc3ODM4NiwiZXhwIjoyMDQwMzU0Mzg2fQ.U3dICm3TzHqfntG47o8P9G-w7lcLI8MX3vTKm78vIng';
+const supabaseUrl = process.env.REACT_APP_SUPABASEURL;
+const supabaseKey = process.env.REACT_APP_SUPABASEKEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
