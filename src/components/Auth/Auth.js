9dcda4f71e2a9d57214b2be8b7971f5bbbf521a6
chaitanya-bhargava <chaitanyabhargava5@gmail.com>
2024-09-08 11:42:49 +0530

    fixed login persist bug and added welcome greeting on dashboard

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,11 +1,26 @@
-import React from "react";
 import SignUp from "./SignUp";
 import Login from "./Login";
 import { useNavigate } from "react-router-dom";
 import './Auth.css';
+import React, { useEffect } from "react";
+import supabase from '../../supabase';
+import { loginSuccess } from "../../actions";
+import { useDispatch } from "react-redux";
 
 const Auth = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    const checkUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      if(user){
+        dispatch(loginSuccess(user));
+        navigate('/dashboard');
+      }
+    }
+    checkUser();
+  },[]);
 
   const goHomeHandler = () => {
     navigate('/');
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import './Dashboard.css'; // Importing the CSS file
 import MySpreadsheets from "../MySpreadsheets/MySpreadsheets";
 import JoinedSpreadsheets from '../JoinedSpreadsheets/JoinedSpreadsheets';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../actions';
 import { useNavigate } from 'react-router-dom';
 import supabase from '../../supabase';
@@ -9,6 +9,7 @@ import supabase from '../../supabase';
 const Dashboard = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const user = useSelector((state) => state.authReducer.user);
 
   const handleLogout = async() => {
     dispatch(logout()); 
@@ -22,14 +23,17 @@ const Dashboard = () => {
         <h1>Dashboard</h1>
         <button className="logout-button" onClick={handleLogout}>Logout</button>
       </div>
-      <div className="dashboard-content">
-        <div className="spreadsheet-section">
-          <h3>My Spreadsheets</h3>
-          <MySpreadsheets />
-        </div>
-        <div className="spreadsheet-section">
-          <h3>Joined Spreadsheets</h3>
-          <JoinedSpreadsheets />
+      <div >
+        <p className='dashboard-greeting'>Welcome <span>{user.email}</span>!</p>
+        <div className="dashboard-content">
+          <div className="spreadsheet-section">
+            <h3>My Spreadsheets</h3>
+            <MySpreadsheets />
+          </div>
+          <div className="spreadsheet-section">
+            <h3>Joined Spreadsheets</h3>
+            <JoinedSpreadsheets />
+          </div>
         </div>
       </div>
     </div>
